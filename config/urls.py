0b252d39f1dcc76a89ab71d8bb62108ac0fe38c4
berylhu43 <beryl.hu@yahoo.com>
2026-08-21 14:50:25 -0500

    minor fix on html, add book non-image placeholder

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('recommendations.urls')),
     path('accounts/', include('accounts.urls')),
     path('catalog/', include('catalog.urls')),
     path('reviews/', include('reviews.urls')),
